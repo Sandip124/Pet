ce68c26c00435a397be3f14f334d08570c83b47f
Sndp124 <chaudharysandip124@gmail.com>
2018-12-27 16:31:59 +0545

    Animal Repository updated

--- Repository/AnimalRepository.cs
+++ Repository/AnimalRepository.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Pet.Data;
 using Pet.Models;
 
@@ -7,33 +8,38 @@ namespace Pet.Repository
     public class AnimalRepository : IAnimalRepository
     {
         private ApplicationDbContext _context;
+
         public AnimalRepository(ApplicationDbContext context)
         {
             _context = context;
         }
-        public void Create(Animal Animal)
+        public void Create(Animal animal)
         {
-            throw new System.NotImplementedException();
+           _context.Animals.Add(animal);
+            _context.SaveChanges();    
         }
 
         public void Delete(Animal animal)
         {
-            throw new System.NotImplementedException();
+            _context.Animals.Remove(animal);
+            _context.SaveChanges();
         }
 
         public void Edit(Animal animal)
         {
-            throw new System.NotImplementedException();
+            _context.Animals.Update(animal);
+            _context.SaveChanges();
         }
 
         public List<Animal> GetAllAnimals()
         {
-            throw new System.NotImplementedException();
+            return _context.Animals.ToList();
         }
 
         public Animal GetSingleAnimal(int id)
         {
-            throw new System.NotImplementedException();
+            var animal = _context.Animals.FirstOrDefault(a=>a.Id == id);
+            return animal;
         }
     }
 }
